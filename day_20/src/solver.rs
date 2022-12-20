fab918ca6e8cca60f45b05f57f0de81836c5f631
Sebastian Woelke <sebastian.woelke@posteo.de>
2022-12-20 22:14:04 +0100

    add day 20 wip

--- day_20/src/solver.rs
+++ day_20/src/solver.rs
@@ -24,48 +24,42 @@ impl CircBuf {
 
     fn mix_idx(&mut self, origin_idx: OriginIdx) {
         let idx = self.get_idx(origin_idx);
-        self.move_element(idx);
+        self.move_elem(idx, self.buf[idx].0);
     }
 
     fn mixed_buf(&self) -> CircBuf {
         let mut res = self.clone();
 
         for origin_idx in 0..self.buf.len() {
+            //println!("{}/{}", origin_idx, self.buf.len());
             res.mix_idx(origin_idx);
-            //println!("{:?}", res.view());
         }
 
         res
     }
 
-    fn move_element(&mut self, idx: Idx) {
-        let item = self.buf[idx];
-        let (val, _) = item;
+    fn move_elem(&mut self, idx: Idx, by: Val) {
+        let circ = (self.buf.len() * 2) as i64;
 
-        let new_val = if val > 0 { val + 1 } else { val };
-        let mut new_idx = self.idx_add(idx, new_val);
-
-        if new_idx == 0 && val < 0 {
-            new_idx = self.buf.len();
-        }
-
-        if new_idx > idx {
-            self.buf.insert(new_idx, item);
-            self.buf.remove(idx);
-        } else if new_idx < idx {
-            self.buf.remove(idx);
-            self.buf.insert(new_idx, item);
+        if by > 0 {
+            self.bubble_elem_forward(idx, by % circ);
+        } else if by < 0 {
+            self.buf.reverse();
+            self.bubble_elem_forward(self.buf.len() - idx - 1, by.abs() % circ);
+            self.buf.reverse();
         }
     }
 
-    fn idx_add(&self, idx: Idx, val: Val) -> Idx {
-        let circ = self.buf.len() as i64;
-        let res = (idx as i64 + val) % circ;
-        if res >= 0 {
-            res as usize
-        } else {
-            (circ + res) as usize
+    fn bubble_elem_forward(&mut self, idx: Idx, by: Val) {
+        if by == 0 {
+            return;
         }
+
+        let c_idx = idx % self.buf.len();
+        let cnext_idx = (idx + 1) % self.buf.len();
+
+        self.buf.swap(c_idx, cnext_idx);
+        self.bubble_elem_forward(idx + 1, by - 1);
     }
 
     fn get_idx(&self, origin_idx: OriginIdx) -> Idx {
@@ -82,9 +76,7 @@ impl CircBuf {
 
     fn nth_val_after_0(&self, nth: usize) -> Val {
         let idx = self.buf.iter().position(|(v, _)| v == &0).unwrap();
-
-        let new_idx = self.idx_add(idx, nth as i64);
-        self.buf[new_idx].0
+        self.buf[(idx + nth) % self.buf.len()].0
     }
 }
 
@@ -101,39 +93,36 @@ pub fn solve_a(loader: &DataLoader) -> Result<String, &str> {
 }
 
 pub fn solve_b(loader: &DataLoader) -> Result<String, &str> {
-    Ok("".to_string())
-}
+    let input_buf = CircBuf::from_loader(loader);
+    let salted_buf = CircBuf::new(
+        input_buf
+            .view()
+            .into_iter()
+            .map(|v| v * 811589153)
+            .collect::<Vec<i64>>(),
+    );
 
-#[cfg(test)]
-mod test_solver {
-    use super::*;
-
-    #[test]
-    fn test_mixed_idx() {
-        let loader = DataLoader::from_file("data/test_input.txt");
-        let mut buf = CircBuf::from_loader(&loader);
-
-        assert_eq!(buf.view(), [1, 2, -3, 3, -2, 0, 4], "Initial arrangement");
-
-        buf.mix_idx(0);
-        assert_eq!(buf.view(), [2, 1, -3, 3, -2, 0, 4], "idx=0; 1 moves between 2 and -3");
-
-        buf.mix_idx(1);
-        assert_eq!(buf.view(), [1, -3, 2, 3, -2, 0, 4], "idx=1; 2 moves between -3 and 3");
-
-        buf.mix_idx(2);
-        assert_eq!(buf.view(), [1, 2, 3, -2, -3, 0, 4], "idx=2; -3 moves between -2 and 0");
-
-        buf.mix_idx(3);
-        assert_eq!(buf.view(), [1, 2, -2, -3, 0, 3, 4], "idx=3; 3 moves between 0 and 4");
-
-        buf.mix_idx(4);
-        assert_eq!(buf.view(), [1, 2, -3, 0, 3, 4, -2], "idx=4; -2 moves between 4 and 1");
-
-        buf.mix_idx(5);
-        assert_eq!(buf.view(), [1, 2, -3, 0, 3, 4, -2], "idx=5; 0 does not move");
-
-        buf.mix_idx(6);
-        assert_eq!(buf.view(), [1, 2, -3, 4, 0, 3, -2], "idx=6; 4 moves between -3 and 0");
+    let mut buf = salted_buf;
+    println!("{:?}\n", buf.view());
+    for i in 1..=10 {
+        let tmp_buf = buf.mixed_buf();
+        buf = tmp_buf;
+        println!("after round {i}");
+        println!("{:?}\n", buf.view());
     }
+
+    let mut res = 0;
+    for nth in [1000, 2000, 3000] {
+        let tmp = buf.nth_val_after_0(nth);
+        println!("tmp={}", tmp);
+        res += tmp
+    }
+
+    Ok(res.to_string())
+
+//tmp=3761715724155
+//tmp=4783506467782
+//tmp=2308971140285
+    //2434767459
+    //811589153
 }
