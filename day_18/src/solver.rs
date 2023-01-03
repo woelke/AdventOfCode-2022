0cd54f0387bed14fcc167d49e2b6160187c7aca2
Sebastian Woelke <sebastian.woelke@posteo.de>
2023-01-04 00:42:09 +0100

    add day 18

--- day_18/src/solver.rs
+++ day_18/src/solver.rs
@@ -1,5 +1,8 @@
 use aoc_helpers::data_loader::DataLoader;
+use cond_utils::Between;
+use itertools::Itertools;
 use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::ops::Add;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
@@ -35,12 +38,7 @@ fn count_free_areas(point: Point, grid: &HashSet<Point>) -> usize {
         .count()
 }
 
-fn is_connected_to_open_area(
-    point: Point,
-    grid: &HashSet<Point>,
-    max_point: Point,
-    depth: usize,
-) -> bool {
+fn is_connected_to_open_area(point: Point, grid: &HashSet<Point>, max_point: Point) -> bool {
     if point.0 == -1
         || point.1 == -1
         || point.2 == -1
@@ -49,29 +47,70 @@ fn is_connected_to_open_area(
         || point.2 == max_point.2
     {
         true
-    } else if depth == 0 {
-        false
     } else {
         get_3d_surrounding()
             .iter()
             .map(|p| point + *p)
             .filter(|p| !grid.contains(p))
-            .map(|p| is_connected_to_open_area(p, grid, max_point, depth - 1))
+            .map(|p| is_connected_to_open_area(p, grid, max_point))
             .any(|x| x)
     }
 }
 
-fn count_areas_connected_to_open_area(point: Point, grid: &HashSet<Point>) -> usize {
-    let max_a = *grid.iter().map(|Point(a, _, _)| a).max().unwrap();
-    let max_b = *grid.iter().map(|Point(_, b, _)| b).max().unwrap();
-    let max_c = *grid.iter().map(|Point(_, _, c)| c).max().unwrap();
-    let max_point = Point(max_a + 1, max_b + 1, max_c + 1);
+fn get_open_grid(grid: &HashSet<Point>) -> HashSet<Point> {
+    let a = grid
+        .iter()
+        .map(|Point(a, _, _)| a)
+        .minmax()
+        .into_option()
+        .unwrap();
+    let b = grid
+        .iter()
+        .map(|Point(_, b, _)| b)
+        .minmax()
+        .into_option()
+        .unwrap();
+    let c = grid
+        .iter()
+        .map(|Point(_, _, c)| c)
+        .minmax()
+        .into_option()
+        .unwrap();
 
+    let min = Point(a.0 - 1, b.0 - 1, c.0 - 1);
+    let max = Point(a.1 + 1, b.1 + 1, c.1 + 1);
+
+    let mut res = HashSet::new();
+
+    let mut jobs = VecDeque::new();
+    jobs.push_back(min);
+    res.insert(min);
+
+    while !jobs.is_empty() {
+        let job = jobs.pop_front().unwrap();
+        for p in get_3d_surrounding().iter().map(|p| job + *p) {
+            let Point(a, b, c) = p;
+            let x = a.within(min.0, max.0);
+            if a.within(min.0, max.0)
+                && b.within(min.1, max.1)
+                && c.within(min.2, max.2)
+                && !grid.contains(&p)
+                && !res.contains(&p)
+            {
+                jobs.push_back(p);
+                res.insert(p);
+            }
+        }
+    }
+
+    res
+}
+
+fn count_open_areas(point: Point, open_areas: &HashSet<Point>) -> usize {
     get_3d_surrounding()
         .iter()
         .map(|p| point + *p)
-        .filter(|p| !grid.contains(p))
-        .filter(|p| is_connected_to_open_area(*p, grid, max_point, max_point.max() as usize))
+        .filter(|p| open_areas.contains(p))
         .count()
 }
 
@@ -104,9 +143,10 @@ pub fn solve_a(loader: &DataLoader, row: i64) -> Result<String, &str> {
 
 pub fn solve_b(loader: &DataLoader, max_xy: i64) -> Result<String, &str> {
     let grid = get_grid(loader);
-    let open_areas = grid
+    let open_grid = get_open_grid(&grid);
+    let count = grid
         .iter()
-        .map(|p| count_areas_connected_to_open_area(*p, &grid))
+        .map(|p| count_open_areas(*p, &open_grid))
         .sum::<usize>();
-    Ok(open_areas.to_string())
+    Ok(count.to_string())
 }
